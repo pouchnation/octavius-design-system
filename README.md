80ee678153127ba5000f4afc5215f66ce1b1126e
Russell Gutierrez <russellraed@gmail.com>
2020-05-05 18:39:20 +0800

    chore: init project

--- README.md
+++ README.md
@@ -2,4 +2,23 @@
 
 ## Getting Started
 
-Running locally
+```
+npm install
+```
+
+## Development
+
+Always edit files inside `scss` folder. Do not modify files in `dist`. To watch and compile changes:
+
+```
+npm run watch
+```
+
+To serve files locally:
+
+```
+npm run start
+```
+
+Then import css in your local project:
+http://localhost:4201/octavius.css
--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const path = require("path");
 
 const app = express();
 
-//here we are configuring dist to serve app files
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/dist"));
 });
 
 const port = 4201;
